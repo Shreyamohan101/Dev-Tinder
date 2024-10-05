3627094ab9152e77c3982456786e4c23fa03d580
Shreya Mohan <shreyamohan.2nov@gmail.com>
2024-10-05 17:27:00 +0530

    using .use route

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
+app.use("/new", (req, res) => {
+    res.end("Hello World, you are at new!");
+        
+    })
 app.use("/test", (req,res) => {
     res.end("Hello sr");
+})
+
+app.use("/sign", (req, res) => {
+ res.end("Hello!! you are at signing page!");
 
 })
+
 app.listen(3000,() => {
     console.log('Server is running on port 3000');
     
