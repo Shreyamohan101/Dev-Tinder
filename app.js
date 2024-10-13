67727adc2f4e7a049067e20ecd80d872e82ff5a8
Shreya Mohan <shreyamohan.2nov@gmail.com>
2024-10-13 16:39:13 +0530

    exploring next()

--- app.js
+++ app.js
@@ -6,19 +6,13 @@ const app = express();
 app.get("/user", (req, res) => {
     res.json({"firstname": "John", "lastname": "apoa"});  // Use res.json to send JSON data
 });
-app.get("/ab?c" , (req, res) => {
-    res.end("Hello World, you are at ab?c!");
+app.use("/new", (req, res, next) => {
+    console.log("New request received");
+    next();
+    },
+    (req, res) => {
+    res.end("Hello World, you are at new!");
 });
-
-    app.post("/user", (req, res) => {
-        res.end("User added successfully");      
-    })
-    app.put("/user", (req,res) => {
-        res.end("User updated successfully");      
-    })
-    app.delete("/user", (req,res) => {
-        res.end("User deleted successfully");      
-    })
 
 // app.use("/test", (req,res) => {
 //     res.end("Hello sr");
