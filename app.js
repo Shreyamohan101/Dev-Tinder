ff9b513673e8f260b4833788f426b961b50956ea
Shreya Mohan <shreyamohan.2nov@gmail.com>
2024-10-14 13:45:57 +0530

    laerning

--- app.js
+++ app.js
@@ -14,13 +14,13 @@ app.use("/new", (req, res, next) => {
     res.end("Hello World, you are at new!");
 });
 
-app.use("/test", (req,res) => {
-    res.end("Hello sr");
-})
+// app.use("/test", (req,res) => {
+//     res.end("Hello sr");
+// })
 
-app.use("/sign", (req, res) => {
- res.end("Hello!! you are at signing page!");
-})
+// app.use("/sign", (req, res) => {
+//  res.end("Hello!! you are at signing page!");
+// })
 
 app.listen(3000,() => {
     console.log('Server is running on port 3000');
